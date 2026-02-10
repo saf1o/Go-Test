f56889740a605ee2121f219b3c96e9fa8d82a1f2
vahkd33114 <vahkd33114@yahoo.co.jp>
2026-02-10 17:11:04 +0900

    update: db.go dsn変更

--- internal/model/db.go
+++ internal/model/db.go
@@ -9,13 +9,16 @@ import (
 var DB *sql.DB
 
 func InitDB(dsn string) error {
+	dsn := "root:bjgkSfio_21751904fordemacia@tcp(localhost:3306)/idle_game?parseTime=true"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return err
 	}
+
 	if err := db.Ping(); err != nil {
 		return err
 	}
+
 	DB = db
 	return nil
 }
