dc3b52b0b9bf05d2a46e5f5d8473e4f75af73329
vahkd33114 <vahkd33114@yahoo.co.jp>
2026-02-10 18:40:40 +0900

    add: db.go 変更

--- internal/model/db.go
+++ internal/model/db.go
@@ -11,15 +11,27 @@ import (
 var DB *sql.DB
 
 func InitDB(dsn string) error {
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	if user == "" || pass == "" || host == "" || port == "" || name == "" {
+		return fmt.Errorf("database environment variables are not set")
+	}
+
 	dsn = fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?parseTime=true",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4",
+		user,
+		pass,
+		host,
+		port,
+		name,
 	)
+
 	db, err := sql.Open("mysql", dsn)
+
 	if err != nil {
 		return err
 	}
