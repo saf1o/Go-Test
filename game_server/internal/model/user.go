f4f37048dafaab46499252782215c2f98c7ae8ab
vahkd33114 <vahkd33114@yahoo.co.jp>
2026-02-13 22:06:24 +0900

    update: user構造体定義　更新

--- game_server/internal/model/user.go
+++ game_server/internal/model/user.go
@@ -7,18 +7,19 @@ import (
 
 // ユーザーデータ構造体定義
 type User struct {
-	UserID        int        `json:"user_id"`         //ユーザーID
-	DeviceID      string     `json:"device_id"`       //デバイスID
-	UserName      string     `json:"user_name"`       //ユーザー名
-	Level         int        `json:"level"`           //レベル
-	Exp           int64      `json:"exp"`             //経験値
-	AttackUp      int        `json:"attack_up"`       //攻撃力アップ
-	SpeedUp       int        `json:"speed_up"`        //攻撃速度アップ
-	HPRegenUp     int        `json:"hp_regen_up"`     //HP回復速度アップ
-	IsIdle        bool       `json:"is_idle"`         //放置中フラグ
-	IdleStartedAt *time.Time `json:"idle_started_at"` //放置開始フラグ
-	CreatedAt     time.Time  `json:"created_at"`      //作成日時
-	UpdatedAt     time.Time  `json:"updated_at"`      //更新日時
+	UserID         int        `json:"user_id"`         //ユーザーID
+	DeviceID       string     `json:"device_id"`       //デバイスID
+	UserName       string     `json:"user_name"`       //ユーザー名
+	Level          int        `json:"level"`           //レベル
+	Exp            int64      `json:"exp"`             //経験値
+	AttackUp       int        `json:"attack_up"`       //攻撃力アップ
+	SpeedUp        int        `json:"speed_up"`        //攻撃速度アップ
+	HPRegenUp      int        `json:"hp_regen_up"`     //HP回復速度アップ
+	EvolutionStage int        `json:"evolution_stage"` //進化段階
+	IsIdle         bool       `json:"is_idle"`         //放置中フラグ
+	IdleStartedAt  *time.Time `json:"idle_started_at"` //放置開始フラグ
+	CreatedAt      time.Time  `json:"created_at"`      //作成日時
+	UpdatedAt      time.Time  `json:"updated_at"`      //更新日時
 }
 
 // UPdateType　強化タイプ
